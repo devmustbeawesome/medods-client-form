67b2e744aa3030fda0b552579106e444e2582066
onton <programm.rechner@gmail.com>
2024-04-03 21:21:44 +0400

    add ui

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,5 +1,7 @@
 import VButton from "@/components/UI/VButton.vue";
 import VInput from "@/components/UI/VInput.vue";
 import VSelect from "@/components/UI/VSelect.vue";
-// import VCheckbox from "@/components/UI/VCheckbox.vue";
-export default [VInput, VButton, VSelect];
+import VCheckbox from "@/components/UI/VCheckbox.vue";
+import VFormAgreement from "@/components/UI/VFormAgreement.vue";
+import VModal from "@/components/UI/VModal.vue";
+export default [VInput, VButton, VSelect, VCheckbox, VFormAgreement, VModal];
