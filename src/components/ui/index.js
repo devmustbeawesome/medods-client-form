adbff9f6dfe34e6aafaa1e61864f8d1ec6bb387f
onton <programm.rechner@gmail.com>
2024-04-11 17:03:33 +0400

    fix ui import

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,7 +1,7 @@
-import VButton from "@/components/UI/VButton.vue";
-import VInput from "@/components/UI/VInput.vue";
-import VSelect from "@/components/UI/VSelect.vue";
-import VCheckbox from "@/components/UI/VCheckbox.vue";
-import VFormAgreement from "@/components/UI/VFormAgreement.vue";
-import VModal from "@/components/UI/VModal.vue";
+import VButton from "./VButton.vue";
+import VInput from "./VInput.vue";
+import VSelect from "./VSelect.vue";
+import VCheckbox from "./VCheckbox.vue";
+import VFormAgreement from "./VFormAgreement.vue";
+import VModal from "./VModal.vue";
 export default [VInput, VButton, VSelect, VCheckbox, VFormAgreement, VModal];
